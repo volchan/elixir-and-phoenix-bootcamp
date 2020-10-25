f6bd65fc0d08a08f4173cfc6b082d6232358b291
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-10-25 11:31:34 +0100

    The Complete Elixir and Phoenix Bootcamp 1/6 done

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,18 +1,6 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for `Cards`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
-  def hello do
-    "Hi there!"
+  def create_deck do
+    ["Ace", "Two", "Three"]
   end
 end
+ 
