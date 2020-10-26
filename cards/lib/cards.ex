ae0a1b6d335a0754c59b259fba18f70eb84d5e76
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-10-26 12:57:32 +0100

    The Complete Elixir and Phoenix Bootcamp 1/13 done

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -3,8 +3,10 @@ defmodule Cards do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Height", "Nine", "Ten", "Jack", "Queen", "King"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-    for suit <- suits do
-      suit
+    for value <- values do
+      for suit <- suits do
+        "#{value} of #{suit}"
+      end
     end
   end
 
