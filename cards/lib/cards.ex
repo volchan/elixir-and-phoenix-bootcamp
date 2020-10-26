372a664856df7b159ba9f4787d950b663c98b0e7
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-10-26 13:09:13 +0100

    The Complete Elixir and Phoenix Bootcamp 1/15 done

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -23,4 +23,8 @@ defmodule Cards do
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size) # {[hand], [rest_of_the_deck]}
+  end
 end
