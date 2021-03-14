379ba0848d639593765b320486bec408fef6964b
stephane de la torre <s.dlt@hotmail.fr>
2021-03-14 16:26:25 +0100

    The Complete Elixir and Phoenix Bootcamp 6/41 done

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -2,6 +2,7 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
   end
 
   def hash_input(input) do
@@ -9,5 +10,11 @@ defmodule Identicon do
       hex: :crypto.hash(:md5, input)
            |> :binary.bin_to_list
     }
+  end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: [red, green, blue | _tail]} = image
+
+    [red, green, blue]
   end
 end
